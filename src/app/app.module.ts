32a5b15a5b2081ae5b3493c353a386b928bac056
michel.brito <michel.brito@dspot.com.pl>
2021-10-03 20:41:51 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FilterSectionComponent } from './filter-section/filter-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterSectionComponent
   ],
   imports: [
     BrowserModule
