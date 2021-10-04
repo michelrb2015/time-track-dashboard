46b43b29dd789550ed82d60c8c44c90e3d41f5fe
michel.brito <michel.brito@dspot.com.pl>
2021-10-03 22:10:28 -0400

    Adding card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FilterSectionComponent } from './filter-section/filter-section.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterSectionComponent
+    FilterSectionComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
