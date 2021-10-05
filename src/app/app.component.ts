3ab4450d9df14658cfa426b656f5edbd849b59e1
michel.brito <michel.brito@dspot.com.pl>
2021-10-05 00:20:28 -0400

    Adding dinamic cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import data from './_files/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'time-track-dashboard-angular';
+  public timeList:any = data;
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() current!: number;
+  @Input() previous!: number;
+  @Input() title!: string;
+
+  route: string = '';
+  background_color: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.route = `assets/images/icon-${this.title.replace(' ', '-').toLowerCase()}.svg`;
+
+    switch (this.title) {
+      case 'Work':
+        this.background_color = 'light-orange'
+        break;
+      case 'Play':
+        this.background_color = 'soft-blue'
+        break;
+      case 'Study':
+        this.background_color = 'light-red'
+
+        break;
+      case 'Exercise':
+        this.background_color = 'lime-green'
+
+        break;
+      case 'Social':
+        this.background_color = 'violet'
+
+        break;
+      case 'Self Care':
+        this.background_color = 'soft-orange'
+
+        break;
+
+      default:
+        break;
+    }
   }
 
 }
+
